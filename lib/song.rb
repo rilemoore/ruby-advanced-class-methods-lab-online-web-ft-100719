e50774d97b42cd5ca2450bc003e12e2a32cc5799
Riley Moore <rilemoore@gmail.com>
2019-10-18 15:46:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,29 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name
+    @artist = artist_name
+    @@all << self
+  end
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    @name = ""
+    @artist_name = ""
+    @@all << self
+    self
+  end
+
+  def self.new_by_name(name_argument)
+    @name = name_argument
   end
 
 end
