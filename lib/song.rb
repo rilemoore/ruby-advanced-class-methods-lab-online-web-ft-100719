cf3267dbd2aa22db6a0e19123f8fe40162a40d75
Riley Moore <rilemoore@gmail.com>
2019-10-18 19:15:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end
   
   def self.find_or_create_by_name(namestring)
-    if self.find_by_name(namestring) == nil
+    if !self.find_by_name(namestring)
       self.create_by_name(namestring)
     else
      self.find_by_name(namestring)
